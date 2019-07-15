d51d1ea67514190e5f07f3e1a20de0a016087aa9
angelo-iOS <angelo.lesano@rsvp.com.au>
2019-07-15 08:59:13 +0800

    hotfix: Temporary fix for crash on multiple selection button tap
    ammendment: Suggested fix did not work. Hiding multiple selection button on viewDidLoad which is the origin of the crash is a more effective solution.

--- Source/Pages/Gallery/YPLibraryVC.swift
+++ Source/Pages/Gallery/YPLibraryVC.swift
@@ -110,6 +110,9 @@ public class YPLibraryVC: UIViewController, YPPermissionCheckable {
 
             strongSelf.updateCropInfo()
         }
+        
+        // Prevent showing multiple selection button initially until permission has been checked
+        v.assetViewContainer.multipleSelectionButton.isHidden = true
     }
     
     public override func viewDidAppear(_ animated: Bool) {
@@ -177,7 +180,6 @@ public class YPLibraryVC: UIViewController, YPPermissionCheckable {
 
         if multipleSelectionEnabled {
             if selection.isEmpty {
-                if mediaManager.fetchResult.count == 0 { return }
                 let asset = mediaManager.fetchResult[currentlySelectedIndex]
                 selection = [
                     YPLibrarySelection(index: currentlySelectedIndex,
